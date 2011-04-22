3f1c3621924dba48c11edf72414f95ab0ac9f775
Suresh Ranganathan <sure.ranga@gmail.com>
2011-04-22 10:32:17 -0400

    updated

--- src/main/java/com/acuvue/exception/InvalidDataException.java
+++
@@ -1,5 +0,0 @@
-package com.acuvue.exception;
-
-public class InvalidDataException  extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-}
--- src/main/java/com/acuvue/exception/InvalidNameException.java
+++
@@ -1,5 +0,0 @@
-package com.acuvue.exception;
-
-public class InvalidNameException  extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-}
